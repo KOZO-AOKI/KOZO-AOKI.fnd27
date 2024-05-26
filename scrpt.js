1128095d7231d65cb1e6b9ed4d445e2592db79f2
KOZO-AOKI <165973277+KOZO-AOKI@users.noreply.github.com>
2024-05-26 18:09:50 +0900

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -5,10 +5,12 @@ let slideIndex = 0;
 function showSlides() {
     const slides = document.querySelectorAll('.slide');
     
+    // 全てのスライドを非表示にする
     slides.forEach(slide => {
         slide.classList.remove('active');
     });
 
+    // 次のスライドを表示
     slideIndex++;
     if (slideIndex > slides.length) {
         // slideIndex = 1;
@@ -18,8 +20,34 @@ function showSlides() {
 
     slides[slideIndex - 1].classList.add('active');
 
-     setTimeout(showSlides, 2000);
+    // 2秒ごとにスライドを切り替える
+    setTimeout(showSlides, 3000);
 }
 
+// スライドショーを開始
 showSlides();
 
+function createGator(event) {
+    console.log(1);
+    let cnt = document.getElementById("num").value;
+    console.log(cnt);
+    let resultString = "";
+    for (let i = 0; i < cnt; i++){
+        resultString += "○";
+    }
+    console.log(resultString);
+    document.getElementById("container2").innerText = resultString;
+}
+
+const heading = document.querySelector("#heading");
+
+const Keyframes = {
+    opacity: [0, 1],
+    translate: ["0 50px",0],
+};
+const options = {
+    duration: 2000,
+    easing: "ease",
+};
+
+heading.animate(Keyframes, options);
