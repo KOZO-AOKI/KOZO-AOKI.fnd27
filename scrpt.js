d4a771c3c8196b96dcf3ad0f416760ba9360804a
KOZO-AOKI <165973277+KOZO-AOKI@users.noreply.github.com>
2024-05-28 14:32:19 +0900

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,43 +1,43 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
-// let slideIndex = 0;
+let slideIndex = 0;
 
-// function showSlides() {
-//     const slides = document.querySelectorAll('.slide');
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
     
-//     // 全てのスライドを非表示にする
-//     slides.forEach(slide => {
-//         slide.classList.remove('active');
-//     });
+    // 全てのスライドを非表示にする
+    slides.forEach(slide => {
+        slide.classList.remove('active');
+    });
 
-//     // 次のスライドを表示
-//     slideIndex++;
-//     if (slideIndex > slides.length) {
-//         // slideIndex = 1;
-//     return;
-//       }
+    // 次のスライドを表示
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        // slideIndex = 1;
+    return;
+      }
 
 
-//     slides[slideIndex - 1].classList.add('active');
+    slides[slideIndex - 1].classList.add('active');
 
-//     // 2秒ごとにスライドを切り替える
-//     setTimeout(showSlides, 3000);
-// }
+    // 2秒ごとにスライドを切り替える
+    setTimeout(showSlides, 3000);
+}
 
-// // スライドショーを開始
-// showSlides();
+// スライドショーを開始
+showSlides();
 
-// function createGator(event) {
-//     console.log(1);
-//     let cnt = document.getElementById("num").value;
-//     console.log(cnt);
-//     let resultString = "";
-//     for (let i = 0; i < cnt; i++){
-//         resultString += "○";
-//     }
-//     console.log(resultString);
-//     document.getElementById("container2").innerText = resultString;
-// }
+function createGator(event) {
+    console.log(1);
+    let cnt = document.getElementById("num").value;
+    console.log(cnt);
+    let resultString = "";
+    for (let i = 0; i < cnt; i++){
+        resultString += "○";
+    }
+    console.log(resultString);
+    document.getElementById("container2").innerText = resultString;
+}
 
 
 
